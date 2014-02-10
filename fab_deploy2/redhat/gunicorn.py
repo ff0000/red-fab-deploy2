e1df00b5c226a66e810a623d384fec88cf042968
Tom Ahn <tomsahn@gmail.com>
2014-02-10 10:59:40 -0800

    redhat gunicorn mods

--- fab_deploy2/redhat/gunicorn.py
+++ fab_deploy2/redhat/gunicorn.py
@@ -30,16 +30,18 @@ class Gunicorn(base_gunicorn.Gunicorn):
         sudo('stop %s' % self.get_name())
 
     def _setup_service(self, env_value=None):
-        conf = os.path.join(env.git_working_dir, 'deploy',
-                                     'gunicorn',
-                                     '%s.conf' % self.gunicorn_name )
+        gunicorn_conf = os.path.join(env.configs_path,
+            "gunicorn/supervisor_{0}.conf".format(
+                self.gunicorn_name))
         # Copy instead of linking so upstart
         # picks up the changes
-        sudo('cp %s /etc/init/' % conf)
+        sudo('cp %s /etc/init/' % gunicorn_conf)
         sudo('initctl reload-configuration')
 
     def upload_templates(self):
         context = super(Gunicorn, self).upload_templates()
+
+        return context
 
     def _setup_rotate(self, path):
         text = [
