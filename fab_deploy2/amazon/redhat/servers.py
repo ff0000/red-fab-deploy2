6723a14fa97487d6f9d24971be9782103062e7f0
markmiscavage <mark.miscavage@ff0000.com>
2017-11-02 16:15:12 -0700

    no collectd for me thanks

--- fab_deploy2/amazon/redhat/servers.py
+++ fab_deploy2/amazon/redhat/servers.py
@@ -4,6 +4,7 @@ from fab_deploy2.operating_systems.redhat.servers import *
 LBServer = amazon_servers.LBServer
 
 class AppServer(amazon_servers.AmazonAppServerMixin, AppServer):
+    setup_collectd = False
     pass
 
 AppServer().as_tasks(name="app_server")
